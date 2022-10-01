e5022c4d90a6962c285bf31d961828f844c248e3
hx210 <76199340+hx210@users.noreply.github.com>
2022-10-01 13:15:19 +0800

    优化

--- entry.sh
+++ entry.sh
@@ -2,10 +2,13 @@
 
 set -e
 
+wireguard_conf_name="warp"
+wireguard_conf_basic_path="/etc/wireguard"
+wireguard_conf_path="${wireguard_conf_basic_path}/${wireguard_conf_name}.conf"
 _downwgcf() {
   echo
   echo "clean up"
-  if ! wg-quick down wgcf; then
+  if ! wg-quick down "${wireguard_conf_name}"; then
     echo "error down"
   fi
   echo "clean up done"
@@ -26,29 +29,30 @@ runwgcf() {
   fi
 
   if [ ! -e "wgcf-profile.conf" ]; then
+    wgcf update
     wgcf generate
   fi
 
-  cp wgcf-profile.conf /etc/wireguard/wgcf.conf
+  ln -sf /wgcf/wgcf-profile.conf "${wireguard_conf_path}"
 
   DEFAULT_GATEWAY_NETWORK_CARD_NAME=$(route | grep default | awk '{print $8}' | head -1)
   DEFAULT_ROUTE_IP=$(ifconfig $DEFAULT_GATEWAY_NETWORK_CARD_NAME | grep "inet " | awk '{print $2}' | sed "s/addr://")
 
-  echo ${DEFAULT_GATEWAY_NETWORK_CARD_NAME}
-  echo ${DEFAULT_ROUTE_IP}
+  echo "${DEFAULT_GATEWAY_NETWORK_CARD_NAME}"
+  echo "${DEFAULT_ROUTE_IP}"
 
-  sed -i "/\[Interface\]/a PostDown = ip rule delete from $DEFAULT_ROUTE_IP  lookup main" /etc/wireguard/wgcf.conf
-  sed -i "/\[Interface\]/a PostUp = ip rule add from $DEFAULT_ROUTE_IP lookup main" /etc/wireguard/wgcf.conf
+  sed -i "/\[Interface\]/a PostDown = ip rule delete from $DEFAULT_ROUTE_IP  lookup main" "${wireguard_conf_path}"
+  sed -i "/\[Interface\]/a PostUp = ip rule add from $DEFAULT_ROUTE_IP lookup main" "${wireguard_conf_path}"
 
   if [ "$1" = "-6" ]; then
-    sed -i 's/AllowedIPs = 0.0.0.0/#AllowedIPs = 0.0.0.0/' /etc/wireguard/wgcf.conf
+    sed -i 's/AllowedIPs = 0.0.0.0/#AllowedIPs = 0.0.0.0/' "${wireguard_conf_path}"
   elif [ "$1" = "-4" ]; then
-    sed -i 's/AllowedIPs = ::/#AllowedIPs = ::/' /etc/wireguard/wgcf.conf
+    sed -i 's/AllowedIPs = ::/#AllowedIPs = ::/' "${wireguard_conf_path}"
   fi
 
   modprobe ip6table_raw
 
-  wg-quick up wgcf
+  wg-quick up "${wireguard_conf_name}"
 
   if [ "$_enableV4" ]; then
     _checkV4
@@ -57,7 +61,7 @@ runwgcf() {
   fi
 
   echo
-  echo "OK, wgcf is up."
+  echo "OK, ${wireguard_conf_name} is up."
 
   echo "配置gost"
   gost -L=:1080
@@ -70,10 +74,10 @@ runwgcf() {
 _checkV4() {
   echo "Checking network status, please wait...."
   while ! curl --max-time 2 ipinfo.io; do
-    wg-quick down wgcf
+    wg-quick down "${wireguard_conf_name}"
     echo "Sleep 2 and retry again."
     sleep 2
-    wg-quick up wgcf
+    wg-quick up "${wireguard_conf_name}"
   done
 
 }
@@ -81,10 +85,10 @@ _checkV4() {
 _checkV6() {
   echo "Checking network status, please wait...."
   while ! curl --max-time 2 -6 ipv6.google.com; do
-    wg-quick down wgcf
+    wg-quick down "${wireguard_conf_name}"
     echo "Sleep 2 and retry again."
     sleep 2
-    wg-quick up wgcf
+    wg-quick up "${wireguard_conf_name}"
   done
 
 }
